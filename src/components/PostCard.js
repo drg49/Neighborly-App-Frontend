0bfd6cc666797c15b4bf065329dd83057afbcd98
Danny Gavin <homevpn14@gmail.com>
2021-07-02 00:55:13 -0700

    Added an edit button that is fully functional

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,15 +1,20 @@
 import { useContext, useState } from 'react'
 import { GlobalCtx } from '../App'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
 const moment = require('moment')
 
 const trash = <FontAwesomeIcon icon={faTrash} />
+const edit = <FontAwesomeIcon icon={faEdit} />
 
 const PostCard = ({username, note, image, realuser, id, date}) => {
 
     const { gState } = useContext(GlobalCtx)
     const { url, token } = gState
+
+    const [editForm, setEditForm] = useState(null)
+    const [currentID, setCurrentID] = useState(null)
+    let idVar; 
 
     const currentUser = localStorage.getItem("user")
 
@@ -31,6 +36,29 @@ const PostCard = ({username, note, image, realuser, id, date}) => {
         }).then(() => window.location.reload())
     }
 
+    const beginUpdate = (id, currentNote) => {
+        setEditForm(<div id="create-caption"><textarea type="text" onChange={handleChange} id="update" value={currentNote} name="note" maxLength="1000"></textarea><br /><button onClick={() => handleUpdate(id)}>Done</button></div>)
+        setCurrentID(id)
+        idVar = id
+    }
+
+    const handleChange = (event) => {
+        setEditForm(<div id="create-caption"><textarea type="text" onChange={handleChange} id="update" value={event.target.value} name="note" maxLength="1000"></textarea><br /><button id="upload-btn" onClick={() => handleUpdate(idVar)}>Done</button></div>)
+    }
+
+    const handleUpdate = (id) => {
+        const note = document.getElementById("update").value
+        fetch(url + "/post/" + id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "bearer " + token
+            },
+            body: JSON.stringify({note})
+        })
+        .then(() => window.location.reload())
+    }
+
 
     return (
         <div id="post-card">
@@ -38,10 +66,19 @@ const PostCard = ({username, note, image, realuser, id, date}) => {
                 <p>{username}</p>
                 <p>{moment(date).format('MM-DD-YYYY')}</p>
             </section>
-        <p id="post-txt">{note}</p>
+        <p id="post-txt">{editForm && currentUser === realuser && currentID === id ? editForm : note}</p>
         {image ? <img src={`https://drg-s3-4.s3.amazonaws.com/${image}`} alt={`Post by ${username}`} id="post-img" /> : null}
-        {currentUser === realuser ? image ? <div id="trash" onClick={() => handleDelete(image)}>{trash}</div> : 
-        <div id="trash" onClick={() => postOnlyDelete(id)}>{trash}</div> : null}
+        {currentUser === realuser ? image ? 
+        <div id="post-flex">
+            <div id="trash" onClick={() => handleDelete(image)}>{trash}</div> 
+            <div id="edit" onClick={() => beginUpdate(id, note)}>{edit}</div>
+        </div>
+        :
+        <div id="post-flex"> 
+            <div id="trash" onClick={() => postOnlyDelete(id)}>{trash}</div> 
+            <div id="edit" onClick={() => beginUpdate(id, note)}>{edit}</div>
+        </div>
+        : null}
         </div>
     )
 }
