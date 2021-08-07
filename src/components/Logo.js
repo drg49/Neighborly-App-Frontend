b7daf4ae33b1f4583f4199b7a3bea1e0e42bf78a
Danny Gavin <homevpn14@gmail.com>
2021-08-07 09:36:20 -0400

    Update 8/7/2021

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import emerald from "./emerald.gif"
 
 const Logo = () => {
-    return <Link to="/"><div id="logo"><h1>Emerald Vaults</h1><img src={emerald} alt="Emerald gif"/></div></Link>
+    return <Link to="/"><div id="logo"><h1>The Vaults</h1><img src={emerald} alt="Emerald gif"/></div></Link>
 }
 
 export default Logo
